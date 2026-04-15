f775c50166b7d905da9236607d46461290c3cf00
Liam-Garvey <61444037+Liam-Garvey@users.noreply.github.com>
2026-04-16 07:59:27 +1000

    added manservant prefab + truth/lies

---
+++ Assets/Scripts/SubjectScripts/ManservantManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class ManservantManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private string dialogue;
+    void Start()
+    {
+        int max = transform.parent.childCount - 1;
+        int accusedNumber = Random.Range(0, max);
+        GameObject accused = gameObject.transform.parent.GetChild(accusedNumber).gameObject;
+        while (gameObject.tag == "Ghost" && accused.tag != "Ghost")
+        {
+            accusedNumber = Random.Range(0, max);
+            accused = gameObject.transform.parent.GetChild(accusedNumber).gameObject;
+        }
+        while (gameObject.tag != "Ghost" && accused.tag == "Ghost")
+        {
+            accusedNumber = Random.Range(0, max);
+            accused = gameObject.transform.parent.GetChild(accusedNumber).gameObject;
+        }
+        dialogue = accused.name + " is telling the truth";
+    }
+    public void OnMouseUpAsButton()
+    {
+        Speak();
+    }
+    public void Speak()
+    {
+        gameObject.GetComponent<CharacterDialogue>().Speak(dialogue);
+    }
+}
