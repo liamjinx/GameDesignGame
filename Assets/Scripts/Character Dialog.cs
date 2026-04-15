738c6e2c5ecbad175db8cce01525263bae041cf9
Liam-Garvey <61444037+Liam-Garvey@users.noreply.github.com>
2026-04-16 03:07:25 +1000

    updated

--- Assets/Scripts/Character Dialog.cs
+++ Assets/Scripts/Character Dialog.cs
@@ -1,16 +1,30 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class CharacterDialogue : MonoBehaviour
 {
     [TextArea]
     public string dialogue;
 
-    public TextMeshProUGUI dialogueText;
+    [SerializeField] private TextMeshProUGUI dialogueText;
 
     public static CharacterDialogue selectedCharacter;
+    public void Start()
+    {
+        
+    }
 
+    public void Update()
+    {
+        
+    }
+
+    public void OnMouseUpAsButton()
+    {
+        Speak();
+    }
     public void Speak()
     {
         dialogueText.text = dialogue;
