e645ea4e673c6ecf0f1cabefb89e40ee1512ec78
Liam-Garvey <61444037+Liam-Garvey@users.noreply.github.com>
2026-04-16 09:58:52 +1000

    changed positions of characters and names

--- Assets/Scripts/SubjectScripts/NunManager.cs
+++ Assets/Scripts/SubjectScripts/NunManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class ClericManager : MonoBehaviour
+public class NunManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private string dialogue;
