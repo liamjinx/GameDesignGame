1c9d0fa77cbb6c146646b0de5383aa140d7ceb1e
Liam-Garvey <61444037+Liam-Garvey@users.noreply.github.com>
2026-04-16 10:40:50 +1000

    added priest and its truths/lies

--- Assets/Scripts/SubjectScripts/PriestManager.cs
+++ Assets/Scripts/SubjectScripts/PriestManager.cs
@@ -14,6 +14,24 @@ public class PriestManager : MonoBehaviour
             accused1Number = Random.Range(0, max);
             accused1 = gameObject.transform.parent.GetChild(accused1Number).gameObject;
         }
+        Debug.Log(accused1Number + " || " + accused1);
+        int accused2Number = Random.Range(0, max);
+        GameObject accused2 = gameObject.transform.parent.GetChild(accused2Number).gameObject;
+        while (accused2Number == accused1Number || accused2.tag == "Ghost")
+        {
+            accused2Number = Random.Range(0, max);
+            accused2 = gameObject.transform.parent.GetChild(accused2Number).gameObject;
+        }
+        Debug.Log(accused2Number + " || " + accused2);
+        int accused3Number = Random.Range(0, max);
+        GameObject accused3 = gameObject.transform.parent.GetChild(accused3Number).gameObject;
+        while (accused3Number == accused2Number || accused3Number == accused1Number || accused2.tag == "Ghost")
+        {
+            accused3Number = Random.Range(0, max);
+            accused3 = gameObject.transform.parent.GetChild(accused3Number).gameObject;
+        }
+        Debug.Log(accused3Number + " || " + accused3);
+        dialogue = "Only one of " + accused1.name + ", " + accused2.name + ", or " + accused3.name + " is a ghost";
     }
     public void OnMouseUpAsButton()
     {
