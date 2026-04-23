a7b33ed7d20352713abbf9a2dcb7edb5125c2fd6
Liam-Garvey <61444037+Liam-Garvey@users.noreply.github.com>
2026-04-23 21:04:41 +1000

    fixed the nun

--- Assets/Scripts/SubjectScripts/NunManager.cs
+++ Assets/Scripts/SubjectScripts/NunManager.cs
@@ -6,7 +6,7 @@ public class NunManager : MonoBehaviour
     private string dialogue;
     void Start()
     {
-        int max = transform.parent.childCount - 1;
+        /*int max = transform.parent.childCount - 1;
         int accused1Number = Random.Range(0, max);
         GameObject accused1 = gameObject.transform.parent.GetChild(accused1Number).gameObject;
         while (accused1.tag == "Ghost" && gameObject.tag == "Ghost")
@@ -17,6 +17,21 @@ public class NunManager : MonoBehaviour
         int accused2Number = Random.Range(0, max);
         GameObject accused2 = gameObject.transform.parent.GetChild(accused2Number).gameObject;
         while (accused1Number == accused2Number || accused2.CompareTag(gameObject.tag))
+        {
+            accused2Number = Random.Range(0, max);
+            accused2 = gameObject.transform.parent.GetChild(accused2Number).gameObject;
+        }*/
+        int max = transform.parent.childCount - 1;
+        int accused1Number = Random.Range(0, max);
+        GameObject accused1 = gameObject.transform.parent.GetChild(accused1Number).gameObject;
+        while (accused1.CompareTag(gameObject.tag))
+        {
+            accused1Number = Random.Range(0, max);
+            accused1 = gameObject.transform.parent.GetChild(accused1Number).gameObject;
+        }
+        int accused2Number = Random.Range(0, max);
+        GameObject accused2 = gameObject.transform.parent.GetChild(accused2Number).gameObject;
+        while ((accused2.tag == "Ghost" && gameObject.tag == "Ghost") || accused2Number == accused1Number || accused2 == gameObject)
         {
             accused2Number = Random.Range(0, max);
             accused2 = gameObject.transform.parent.GetChild(accused2Number).gameObject;
