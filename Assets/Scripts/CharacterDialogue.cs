31cc6e5c7a5d3a5aa7ed269595afbceb76cbfcc3
liamjinx <liamdavies228@gmail.com>
2026-04-30 15:21:47 +1000

    Code without lives code as it wasnt working

--- Assets/Scripts/CharacterDialogue.cs
+++ Assets/Scripts/CharacterDialogue.cs
@@ -14,25 +14,6 @@ public class CharacterDialogue : MonoBehaviour
     private Canvas beatStage;
     private CharacterManager ghostCount;
     
-    
-    // lives code 
-    [SerializeField] private Sprite NoLife;
-
-    private static Image Life1;
-    private static Image Life2;
-    private static Image Life3;
-
-    private static int lives = 3;
-    
-    [SerializeField] private Sprite FullLife;
-   
-    private static Sprite noLifeStatic;
-    private static Sprite fullLifeStatic;
-    
-    [SerializeField] private bool isTutorial = false;
-    
-    //end of lives
-    
     public string lastSpokenLine = "";
     public string characterName;
     
@@ -45,18 +26,6 @@ public class CharacterDialogue : MonoBehaviour
         beatStage = GameObject.FindGameObjectWithTag("BeatStage").GetComponent<Canvas>();
         ghostCount = GetComponentInParent<CharacterManager>();
         mainText = UICanvas.GetComponentInChildren<TextMeshProUGUI>();
-        
-        if (noLifeStatic == null && NoLife != null)
-            noLifeStatic = NoLife;
-
-        if (fullLifeStatic == null && FullLife != null)
-            fullLifeStatic = FullLife;
-        
-        Life1 = GameObject.Find("Life1")?.GetComponent<Image>();
-        Life2 = GameObject.Find("Life2")?.GetComponent<Image>();
-        Life3 = GameObject.Find("Life3")?.GetComponent<Image>();
-        
-        UpdateHeartsVisual();
         
     }
     public void Petrify()
@@ -91,16 +60,10 @@ public class CharacterDialogue : MonoBehaviour
                 {
                     anim.playPetrifyAnimation();
                 }
-
-                // lose life FIRST
-                LoseLife();
-
+                
                 // show popup
                 UICanvas.enabled = false;
                 lostStage.enabled = true;
-
-                // wait then restart
-                Invoke(nameof(RestartLevel), 1.5f);
             }
             //Destroy(gameObject);
             return;
@@ -114,55 +77,7 @@ public class CharacterDialogue : MonoBehaviour
         lastSpokenLine = dialogue;
         characterName = gameObject.name;
     }
-
-    private void LoseLife()
-    {
-        if (SceneManager.GetActiveScene().name == "Tutorial") 
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            return;
-        }
-
-        if (lives <= 0) return;
-        if (noLifeStatic == null) return;
-
-        lives--;
-
-        Debug.Log("Lives now: " + lives);
-
-        if (lives == 2 && Life3 != null)
-            Life3.sprite = noLifeStatic;
-
-        else if (lives == 1 && Life2 != null)
-            Life2.sprite = noLifeStatic;
-
-        else if (lives == 0 && Life1 != null)
-            Life1.sprite = noLifeStatic;
-    }
     
-    void RestartLevel()
-    {
-        if (lives <= 0)
-        {
-            lives = 3;
-            SceneManager.LoadScene("Level2Stage1");
-        }
-        else
-        {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
-    }
     
-    void UpdateHeartsVisual()
-    {
-        if (lives <= 2 && Life3 != null)
-            Life3.sprite = noLifeStatic;
-
-        if (lives <= 1 && Life2 != null)
-            Life2.sprite = noLifeStatic;
-
-        if (lives <= 0 && Life1 != null)
-            Life1.sprite = noLifeStatic;
-    }
-    
+   
 }
