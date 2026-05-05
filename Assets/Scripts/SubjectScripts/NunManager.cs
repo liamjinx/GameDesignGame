5af2d8650d13cddfa446b9cb30edf6b118d4c981
Liam-Garvey <61444037+Liam-Garvey@users.noreply.github.com>
2026-05-06 01:32:48 +1000

    reworked nun

--- Assets/Scripts/SubjectScripts/NunManager.cs
+++ Assets/Scripts/SubjectScripts/NunManager.cs
@@ -4,33 +4,48 @@ public class NunManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private string dialogue;
+    private int max;
+    private int accused1Number;
+    private GameObject accused1;
+    private int accused2Number;
+    private GameObject accused2;
     void Start()
     {
-        int max = transform.parent.childCount - 1;
-        int accused1Number = Random.Range(0, max);
-        GameObject accused1 = gameObject.transform.parent.GetChild(accused1Number).gameObject;
-        while (accused1.CompareTag(gameObject.tag))
-        {
-            accused1Number = Random.Range(0, max);
-            accused1 = gameObject.transform.parent.GetChild(accused1Number).gameObject;
-        }
-        int accused2Number = Random.Range(0, max);
-        GameObject accused2 = gameObject.transform.parent.GetChild(accused2Number).gameObject;
-        while ((accused2.tag == "Ghost" && gameObject.tag == "Ghost") || accused2Number == accused1Number || accused2 == gameObject)
+        max = transform.parent.childCount - 1;
+        accused1Number = Random.Range(0, max);
+        accused1 = gameObject.transform.parent.GetChild(accused1Number).gameObject;
+        accused2Number = Random.Range(0, max);
+        accused2 = gameObject.transform.parent.GetChild(accused2Number).gameObject;
+        if (gameObject.tag != "Untagged") { SpeakLies(); }
+        else { SpeakTruth(); Debug.Log(gameObject.tag); }
+        dialogue = accused1.name + " and " + accused2.name + " could be ghosts";
+        gameObject.GetComponent<CharacterDialogue>().Speak(dialogue);
+        while ((accused2.tag == "Ghost" && gameObject.tag != "Untagged") || accused2Number == accused1Number || accused2 == gameObject)
         {
             accused2Number = Random.Range(0, max);
             accused2 = gameObject.transform.parent.GetChild(accused2Number).gameObject;
         }
-        dialogue = accused1.name + " and " + accused2.name + " could be ghosts";
-        Speak();
     }
     
     public void OnMouseUpAsButton()
     {
         gameObject.GetComponent<CharacterDialogue>().Petrify();
     }
-    public void Speak()
+    public void SpeakTruth()
     {
-        gameObject.GetComponent<CharacterDialogue>().Speak(dialogue);
+        while (accused1.tag == "Untagged")
+        {
+            accused1Number = Random.Range(0, max);
+            accused1 = gameObject.transform.parent.GetChild(accused1Number).gameObject;
+        }
+    }
+    public void SpeakLies()
+    {
+        Debug.Log(gameObject.tag);
+        while (accused1.tag == "Ghost")
+        {
+            accused1Number = Random.Range(0, max);
+            accused1 = gameObject.transform.parent.GetChild(accused1Number).gameObject;
+        }
     }
 }
