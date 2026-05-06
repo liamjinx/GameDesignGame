1f8905cef5101e83d52171b6ca1b861fd2e6dd4a
zahlia100-ops <zahlia100@gmail.com>
2026-05-06 23:44:20 +1000

    added level 3 stage 4

---
+++ Assets/Scripts/LevelScripts/Level3Stage4.cs
@@ -0,0 +1,147 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level3Stage4 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private GameObject ghost1;
+    private GameObject ghost2;
+    private GameObject ghost3;
+
+    private static bool hasShownStartExplanation;
+
+    [SerializeField] private GameObject noteButton;
+    [SerializeField] private GameObject ghostCount;
+    [SerializeField] private GameObject petrifyButton;
+    [SerializeField] private GameObject timer;
+    [SerializeField] private GameObject timerExplaination;
+
+    private CharacterManager characterManager;
+    private int max;
+    void Awake()
+    {
+        characterManager = GetComponent<CharacterManager>();
+        max = transform.childCount - 1;
+        int ghostnumber1 = Random.Range(0, max);
+        int ghostnumber2 = Random.Range(0, max);
+        int ghostnumber3 = Random.Range(0, max);
+        while (ghostnumber1 == ghostnumber2 || ghostnumber1 == ghostnumber3 || ghostnumber2 == ghostnumber3)
+        {
+            ghostnumber2 = Random.Range(0, max);
+            ghostnumber3 = Random.Range(0, max);
+        }
+        ghost1 = transform.GetChild(ghostnumber1).gameObject;
+        ghost2 = transform.GetChild(ghostnumber2).gameObject;
+        ghost3 = transform.GetChild(ghostnumber3).gameObject;
+        ghost1.tag = "Ghost";
+        ghost2.tag = "Ghost";
+        ghost3.tag = "Ghost";
+        ActivatePhantom();
+        ActivateWraith();
+        ActivateNuckelavee();
+        for (int i = 0; i <= max; ++i)
+        {
+            GameObject character = transform.GetChild(i).gameObject;
+            if (character.tag == "Ghost") { characterManager.ghosts.Add(character); }
+            else { characterManager.subjects.Add(character); }
+            if (character.tag == "Untagged") { characterManager.honest.Add(character); }
+            else if (character.tag == "Lying") { characterManager.lying.Add(character); }
+        }
+    }
+
+    private void ActivatePhantom()
+    {
+        int currentPos = ghost1.transform.GetSiblingIndex();
+        int beforePos; int afterPos;
+        if (currentPos == 0) { beforePos = max; afterPos = currentPos + 1; }
+        else if (currentPos == max) { beforePos = currentPos - 1; afterPos = 0; }
+        else { beforePos = currentPos - 1; afterPos = currentPos + 1; }
+        if (transform.GetChild(beforePos).gameObject.tag == "Untagged")
+        {
+            transform.GetChild(beforePos).gameObject.tag = "Lying";
+        }
+        else if (transform.GetChild(afterPos).gameObject.tag == "Untagged")
+        {
+            transform.GetChild(afterPos).gameObject.tag = "Lying";
+        }
+    }
+
+    private void ActivateWraith()
+    {
+        int affected = Random.Range(0, max);
+        GameObject affectedSubject = transform.GetChild(affected).gameObject;
+        while (affectedSubject.tag != "Untagged")
+        {
+            affected = Random.Range(0, max);
+            affectedSubject = transform.GetChild(affected).gameObject;
+        }
+        affectedSubject.tag = "Lying";
+    }
+
+    private void ActivateNuckelavee()
+    {
+        int currentPos = ghost1.transform.GetSiblingIndex();
+        int beforePos; int afterPos;
+        if (currentPos == 0) { beforePos = max; afterPos = currentPos + 1; }
+        else if (currentPos == max) { beforePos = currentPos - 1; afterPos = 0; }
+        else { beforePos = currentPos - 1; afterPos = currentPos + 1; }
+        if (transform.GetChild(beforePos).gameObject.tag == "Untagged")
+        {
+            transform.GetChild(beforePos).gameObject.tag = "Lying";
+        }
+        if (transform.GetChild(afterPos).gameObject.tag == "Untagged")
+        {
+            transform.GetChild(afterPos).gameObject.tag = "Lying";
+        }
+    }
+
+    private bool isLoading = false;
+    public void PlayAgain()
+    {
+        if (isLoading) return; // prevents double click
+
+        isLoading = true;
+
+        Debug.Log("PlayAgain clicked");
+
+        CharacterDialogue cd = FindAnyObjectByType<CharacterDialogue>();
+
+        if (cd != null && cd.IsGameOver())
+        {
+            cd.ResetLives();
+            SceneManager.LoadScene(9); // back to stage 1
+        }
+        else
+        {
+            SceneManager.LoadScene(12); // retry stage 4
+        }
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
+    }
+
+    void Start()
+    {
+            HideTimerExplanation();
+    }
+    
+
+    public void ShowTimerExplanation()
+    {
+        noteButton.SetActive(false);
+        ghostCount.SetActive(false);
+        petrifyButton.SetActive(false);
+        timer.SetActive(false);
+        timerExplaination.SetActive(true);
+    }
+    public void HideTimerExplanation()
+    {
+        noteButton.SetActive(true);
+        ghostCount.SetActive(true);
+        petrifyButton.SetActive(true);
+        timer.SetActive(true);
+        timerExplaination.SetActive(false);
+    }
+}
