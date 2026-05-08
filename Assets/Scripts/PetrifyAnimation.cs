658760019a9e1a5f3f7eebdecdb4076fc3c5fe88
zahlia100-ops <zahlia100@gmail.com>
2026-05-09 00:51:03 +1000

    implemented some phantom animations

--- Assets/Scripts/PetrifyAnimation.cs
+++ Assets/Scripts/PetrifyAnimation.cs
@@ -11,10 +11,24 @@ public class PetrifyAnimation : MonoBehaviour
             animator = GetComponent<Animator>();
         }
 
-        if (gameObject.tag == "Ghost")
-        {
-            animator.SetTrigger("PetrifyGhost");
-        }
+        ///if (gameObject.tag == "Ghost")
+        //{
+            //animator.SetTrigger("PetrifyGhost");
+            if (gameObject.layer == LayerMask.NameToLayer("Phantom"))
+            {
+                animator.SetTrigger("PetrifyPhantom");
+            } else if (gameObject.layer == LayerMask.NameToLayer("Wraith"))
+            {
+                animator.SetTrigger("PetrifyWraith");
+            } else if (gameObject.layer == LayerMask.NameToLayer("Nuckelavee"))
+            {
+                animator.SetTrigger("PetrifyNuckelavee");
+            } else if (gameObject.tag == "Ghost")
+            {
+                animator.SetTrigger("PetrifyGhost");
+            }
+            
+        //}
         
         else
         {
